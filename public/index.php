0f9908486dd56bd45258b0dbf96aae460075d793
gitspathe <55875553+Gitspathe@users.noreply.github.com>
2022-09-05 20:56:11 +0930

    Added material css

--- public/index.php
+++ public/index.php
@@ -3,10 +3,35 @@ require("../app/Helper/Constants.php");
 ?>
 
 <!DOCTYPE html>
-<html>
-<body>
+<html lang="en">
 
-<h1>NUBER</h1>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Nuber. Uber - but with an N in front of it.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+
+    <link rel="stylesheet" href="Style/material.min.css">
+    <link rel="stylesheet" href="Style/material_icons.css">
+    <link rel="stylesheet" href="Style/home_style.css">
+    <script src="Style/material.min.js"></script>
+</head>
+
+<body>
+    <div class="mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
+      <main class="main mdl-layout__content">
+        <div class="container mdl-grid">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+          <div class="content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+            <h3>Nuber</h3>
+              <p>
+                Cillum dolor esse sit incididunt velit eiusmod magna ad nostrud officia aute dolor dolor. Magna esse ullamco pariatur adipisicing consectetur eu commodo officia. Ex cillum consequat mollit minim elit est deserunt occaecat nisi amet. Quis aliqua nostrud Lorem occaecat sunt. Eiusmod quis amet ullamco aliquip dolore ut incididunt duis adipisicing. Elit consequat nisi eiusmod aute ipsum sunt veniam do est. Occaecat mollit aliquip ut proident consectetur amet ex dolore consectetur aliqua elit.
+              </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  </body>
 
 </body>
 </html>
